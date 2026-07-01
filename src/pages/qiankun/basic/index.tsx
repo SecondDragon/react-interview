07842720246295cedbd876c5ce87b70c03712382
涅炎 <50282797+SecondDragon@users.noreply.github.com>
2026-07-01 10:51:25 +0800

    feat(qiankun): add qiankun basic page entry component

---
+++ src/pages/qiankun/basic/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Typography, Space, Divider } from 'antd';
+import { pageData } from './data';
+import LiveDemo from './LiveDemo';
+import SectionIntro from './chapters/SectionIntro';
+import SectionChildBuild from './chapters/SectionChildBuild';
+import SectionChildEntry from './chapters/SectionChildEntry';
+import SectionChildRouter from './chapters/SectionChildRouter';
+import SectionHostRegister from './chapters/SectionHostRegister';
+import SectionMountContainer from './chapters/SectionMountContainer';
+
+const QiankunBasicPage: React.FC = () => {
+  return (
+    <div style={{ maxWidth: 1200, margin: '0 auto', padding: 24 }}>
+      <Typography.Title>{pageData.title}</Typography.Title>
+      <Typography.Paragraph type="secondary">{pageData.subtitle}</Typography.Paragraph>
+
+      <Divider />
+
+      <Space direction="vertical" size="large" style={{ width: '100%' }}>
+        <SectionIntro />
+
+        <LiveDemo />
+
+        <SectionChildBuild />
+        <SectionChildEntry />
+        <SectionChildRouter />
+        <SectionHostRegister />
+        <SectionMountContainer />
+      </Space>
+    </div>
+  );
+};
+
+export default QiankunBasicPage;
